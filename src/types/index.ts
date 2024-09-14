41b2da2013f32d5185da4b5ea04854d2f515c2ea
Nikita Maltsev <djenot330@mail.ru>
2024-09-14 23:41:56 +0500

    сделал Документацию

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+// Интерфейсы и типы для бизнес-логики
+
+// Тип связанный с товарами
+
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+// Типы для работы с заказом
+
+export type TOrderInvoice = Omit<IOrder, 'items'> & {
+	items: string[];
+	total: number;
+};
+
+export type TOrderPayment = 'cash' | 'card';
+
+export type TOrderStep = 'shipment' | 'contacts';
+
+export interface IOrder {
+	items: IProduct[];
+	payment: TOrderPayment;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
